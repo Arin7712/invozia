bdaa164abd62b58c4f8d7ae22f26edbbe41eae01
Arin7712 <aringawande7712@gmail.com>
2025-04-14 00:59:24 +0530

    kk

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -6,14 +6,14 @@ import Image from 'next/image'
 const Testimonials = () => {
   return (
 <div className="md:px-[22rem] px-6 md:py-[6rem] py-[2rem]">
-      <div className="flex md:flex-row flex-col justify-between items-center md:text-start text-center">
+      <div className="flex md:flex-row flex-col md:gap-0 gap-4 justify-between items-center md:text-start text-center">
         <h1 className="text-3xl font-medium md:max-w-2xl">Testimonials</h1>
         <Button className="rounded-full">Browse articles</Button>
       </div> 
       <div className="flex flex-col pt-10 gap-[4rem]">
                       {
                           testimonialItems.map((item, index) => (
-                              <div key={index} className="rounded-md border-[1px] p-6 flex md:flex-row flex-col gap-6 items-center md:items-start text-start">
+                              <div key={index} className="rounded-md border-[1px] p-6 flex md:flex-row flex-col gap-6 items-start text-start">
                                   <div className='px-4 py-2 bg-neutral-900 rounded-full border-[1px] text-sm'>
                                       {item.tag}
                                   </div>
